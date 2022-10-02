308b7d94849a51e04b77fdbe3729588321f4b411
Shasmit <shasmitbasnet606@gmail.com>
2022-10-02 20:08:34 +0545

    My website

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+(function ($) {
+
+  "use strict";
+
+    // COLOR MODE
+    $('.color-mode').click(function(){
+        $('.color-mode-icon').toggleClass('active')
+        $('body').toggleClass('dark-mode')
+    })
+
+    // HEADER
+    $(".navbar").headroom();
+
+    // PROJECT CAROUSEL
+    $('.owl-carousel').owlCarousel({
+    	items: 1,
+	    loop:true,
+	    margin:10,
+	    nav:true
+	});
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.nav-link, .custom-btn-link').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    });  
+
+    // TOOLTIP
+    $('.social-links a').tooltip();
+
+
+    $(document).ready(function() {
+      $('#buttonsend').click(function() {
+          $('#fr1').attr('action',
+                         'mailto:shasmitbasnet606@gmail.com?subject=' +
+                         $('#name').val() + '&body=' + $('#message').val());
+          $('#fr1').submit();
+      });
+  });
+
+})(jQuery);
